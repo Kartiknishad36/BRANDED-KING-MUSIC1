cc2c846e500274e2a97204ea4af69b697fc9b26f
Kartiknishad36 <164469009+Kartiknishad36@users.noreply.github.com>
2024-03-23 00:57:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,11 +17,11 @@ PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/9db6ae360e0a7b833abff.jp
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/varmasarkar")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/varmasarkar")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "BRANDED KIGN")  
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "VARMA SARKAR")  
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
